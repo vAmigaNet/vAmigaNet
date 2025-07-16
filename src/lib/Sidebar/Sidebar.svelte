a526550ade20d4dc770cd8697aac959d80ce310b
Dirk Hoffmann <dirk.hoffmann@me.com>
2025-07-16 15:47:56 +0200

    cleanup

--- src/lib/Sidebar/Sidebar.svelte
+++ src/lib/Sidebar/Sidebar.svelte
@@ -1,130 +1,121 @@
 <script lang="ts">
-    import { run } from 'svelte/legacy';
+	import { Layer } from '$lib/types';
+	import SidebarButton from '$lib/Sidebar/SidebarButton.svelte';
+	import SidebarSection from '$lib/Sidebar/SidebarSection.svelte';
+	import { fade } from 'svelte/transition';
+	import SidebarPad from '$lib/Sidebar/SidebarPad.svelte';
+	import {
+		debugDma,
+		layer,
+		layout,
+		port1,
+		port2,
+		poweredOn,
+		running,
+		showSidebar
+	} from '$lib/stores';
 
-    import { Layer } from '$lib/types';
-    import SidebarButton from '$lib/Sidebar/SidebarButton.svelte';
-    import SidebarSection from '$lib/Sidebar/SidebarSection.svelte';
-    import { fade } from 'svelte/transition';
-    import { debugDma, layer, layout, port1, port2, poweredOn, running, showSidebar } from '$lib/stores';
-    import SidebarPad from '$lib/Sidebar/SidebarPad.svelte';
-
-    let {
+	let {
 		action = () => {}
 	}: {
 		action: (sender: string, state: boolean) => void;
 	} = $props();
 
-    let sel = $state('');
-    let duration = 200;
+	let sel = $state('');
+	let duration = 200;
 
-    function select(sender: string, state: boolean)
-    {
-        console.log('select', sender, state);
-        sel = sender == sel ? '' : sender;
-        console.log('sender: ', sender, ' state: ', state);
-        action(sender, state);
-    }
-
-    const portIcons = [
-        'icons/device-none.png',
-        'icons/device-mouse.png',
-        'icons/device-keyset-1.png',
-        'icons/device-keyset-2.png',
-        'icons/device-usb-1.png',
-        'icons/device-usb-2.png'
-    ]
-
-    const power = {id: 'power', icon: 'icons/powerIcon.png'};
-    const pause = $state({id: 'pause', icon: 'icons/pauseIcon.png'});
-    const reset = {id: 'reset', icon: 'icons/resetIcon.png'};
-    const settings = {id: 'settings', icon: 'icons/settingsIcon.png'};
-    const port1Button = $state({id: 'port1', icon: 'icons/device-none.png'});
-    const port2Button = $state({id: 'port2', icon: 'icons/device-none.png'});
-    const port1Items = [
-        {id: 'empty1', icon: portIcons[0]},
-        {id: 'mouse1', icon: portIcons[1]},
-        {id: 'keyset11', icon: portIcons[2]},
-        {id: 'keyset12', icon: portIcons[3]},
-        {id: 'gamepad11', icon: portIcons[4]}
-    ];
-    const port2Items = [
-        {id: 'empty2', icon: portIcons[0]},
-        {id: 'mouse2', icon: portIcons[1]},
-        {id: 'keyset21', icon: portIcons[2]},
-        {id: 'keyset22', icon: portIcons[3]},
-        {id: 'gamepad21', icon: portIcons[4]}
-    ];
-    const shell = {id: 'shell', icon: 'icons/retroShellIcon.png'};
-    const monitor = {id: 'monitor', icon: 'icons/monitorIcon.png'};
-    const layoutButton = $state({id: 'layout', icon: 'icons/layoutIcon.png'});
-    const layoutItems = [
-        {id: 'aspect', icon: 'icons/layoutAspectIcon.png'},
-        {id: 'fit', icon: 'icons/layoutFitIcon.png'},
-        {id: 'full', icon: 'icons/layoutFullIcon.png'}
-    ];
-
-    run(() => {
-        port1Button.icon = portIcons[$port1];
-    });
-    run(() => {
-        port2Button.icon = portIcons[$port2];
-    });
-    run(() => {
-        layoutButton.icon =
-            $layout == 'full'
-                ? 'icons/layoutFullIcon.png'
-                : $layout == 'aspect'
-                    ? 'icons/layoutAspectIcon.png'
-                    : 'icons/layoutFitIcon.png';
-    });
-
-    run(() => {
-        if ($running) {
-            pause.icon = 'icons/pauseIcon.png';
-        } else {
-            pause.icon = 'icons/runIcon.png';
-        }
-    });
+	function select(sender: string, state: boolean) {
+		sel = sender == sel ? '' : sender;
+		action(sender, state);
+	}
+	const portIcons = [
+		'icons/device-none.png',
+		'icons/device-mouse.png',
+		'icons/device-keyset-1.png',
+		'icons/device-keyset-2.png',
+		'icons/device-usb-1.png',
+		'icons/device-usb-2.png'
+	];
+	let powerIcon = 'icons/powerIcon.png';
+	let resetIcon = 'icons/resetIcon.png';
+	let settingsIcon = 'icons/settingsIcon.png';
+	let shellIcon = 'icons/retroShellIcon.png';
+	let monitorIcon = 'icons/monitorIcon.png';
+	let pauseIcon = $derived($running ? 'icons/pauseIcon.png' : 'icons/runIcon.png');
+	let port1Icon = $derived(portIcons[$port1]);
+	let port2Icon = $derived(portIcons[$port2]);
+	let layoutIcon = $derived(
+		$layout == 'full'
+			? 'icons/layoutFullIcon.png'
+			: $layout == 'aspect'
+				? 'icons/layoutAspectIcon.png'
+				: 'icons/layoutFitIcon.png'
+	);
+	const port1Items = [
+		{ id: 'empty1', icon: portIcons[0] },
+		{ id: 'mouse1', icon: portIcons[1] },
+		{ id: 'keyset11', icon: portIcons[2] },
+		{ id: 'keyset12', icon: portIcons[3] },
+		{ id: 'gamepad11', icon: portIcons[4] }
+	];
+	const port2Items = [
+		{ id: 'empty2', icon: portIcons[0] },
+		{ id: 'mouse2', icon: portIcons[1] },
+		{ id: 'keyset21', icon: portIcons[2] },
+		{ id: 'keyset22', icon: portIcons[3] },
+		{ id: 'gamepad21', icon: portIcons[4] }
+	];
+	const layoutItems = [
+		{ id: 'aspect', icon: 'icons/layoutAspectIcon.png' },
+		{ id: 'fit', icon: 'icons/layoutFitIcon.png' },
+		{ id: 'full', icon: 'icons/layoutFullIcon.png' }
+	];
 </script>
 
 {#if $showSidebar}
-    <div transition:fade={{ duration }} class="">
-        <div class="absolute top-0 left-0 bg-base-100 bg-opacity-80 flex flex-col w-16 pl-2 z-40">
-            <div class="flex flex-col space-y-1.5 mt-1.5 mb-2 border-0">
-                <SidebarButton {select} item={power}/>
-                {#if $poweredOn}
-                    <SidebarButton {select} item={pause}/>
-                    <SidebarButton {select} item={reset}/>
-                {/if}
-                <SidebarPad/>
-
-                <SidebarSection
-                        {select}
-                        expanded={sel === 'port1'}
-                        item={port1Button}
-                        subitems={port1Items}
-                />
-                <SidebarSection
-                        {select}
-                        expanded={sel === 'port2'}
-                        item={port2Button}
-                        subitems={port2Items}
-                />
-                <SidebarPad/>
-
-                <SidebarButton {select} item={settings} active={$layer === Layer.settings}/>
-                <SidebarButton {select} item={shell} active={$layer === Layer.shell}/>
-                <SidebarPad/>
-                {#if $poweredOn}
-                    <SidebarSection
-                            {select}
-                            expanded={sel === 'layout'}
-                            item={layoutButton}
-                            subitems={layoutItems}
-                    />
-                    <SidebarButton {select} item={monitor} active={$debugDma}/>
-                {/if}
-            </div>
-        </div>
-    </div>
+	<div transition:fade={{ duration }} class="">
+		<div class="bg-base-100 bg-opacity-80 absolute top-0 left-0 z-40 flex w-16 flex-col pl-2">
+			<div class="mt-1.5 mb-2 flex flex-col space-y-1.5 border-0">
+				<SidebarButton {select} item={{ id: 'power', icon: powerIcon }} />
+				{#if $poweredOn}
+					<SidebarButton {select} item={{ id: 'pause', icon: pauseIcon }} />
+					<SidebarButton {select} item={{ id: 'reset', icon: resetIcon }} />
+				{/if}
+				<SidebarPad />
+				<SidebarSection
+					{select}
+					expanded={sel === 'port1'}
+					item={{ id: 'port1', icon: port1Icon }}
+					subitems={port1Items}
+				/>
+				<SidebarSection
+					{select}
+					expanded={sel === 'port2'}
+					item={{ id: 'port2', icon: port2Icon }}
+					subitems={port2Items}
+				/>
+				<SidebarPad />
+				<SidebarButton
+					{select}
+					item={{ id: 'settings', icon: settingsIcon }}
+					active={$layer === Layer.settings}
+				/>
+				<SidebarButton
+					{select}
+					item={{ id: 'shell', icon: shellIcon }}
+					active={$layer === Layer.shell}
+				/>
+				<SidebarPad />
+				{#if $poweredOn}
+					<SidebarSection
+						{select}
+						expanded={sel === 'layout'}
+						item={{ id: 'layout', icon: layoutIcon }}
+						subitems={layoutItems}
+					/>
+					<SidebarButton {select} item={{ id: 'monitor', icon: monitorIcon }} active={$debugDma} />
+				{/if}
+			</div>
+		</div>
+	</div>
 {/if}
